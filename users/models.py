494b47bd67435bf3ee60106098cee0293763151f
maxsyrnik03 <114486240+maxsyrnik03@users.noreply.github.com>
2024-04-04 19:21:24 +0100

    test

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    STATUS = {
+        ('regular','regular'),
+        ('moderator','moderator')
+    }
+
+    email = models.EmailField(unique=True)
+    status = models.CharField(max_length=100,choices=STATUS, default='regular')
+
+    def __str__(self):
+        return self.username
